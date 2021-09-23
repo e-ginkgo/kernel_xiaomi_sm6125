3f2b1846b3e9906af854e497670670278fb5a587
Harsh  Pal <iamhp2k@gmail.com>
2021-09-23 11:18:27 +0000

    Revert "RTIC: Move selinux_state to a separate 4k"
    This reverts commit 16431697784f395f984fa7b09767d45b47335790.

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Sep 23 11:10:18 UTC 2021"
+#define LINUX_COMPILE_BY "gitpod"
+#define LINUX_COMPILE_HOST "ws-f4426f4f-27c9-4d10-821e-6300b6138c12"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a), GNU ld (GNU Binutils) 2.36.1"
